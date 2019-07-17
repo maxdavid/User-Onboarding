104321aa499223a1c818b642c6d638165b6033b0
maxdavid <me@maxdavid.com>
2019-07-17 15:18:45 -0700

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return null;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
