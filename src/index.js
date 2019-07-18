9f4a69f39b7dd9906cf52d3d47d407b9b76f60f1
maxdavid <me@maxdavid.com>
2019-07-17 19:15:11 -0700

    Simple onboarding form with validation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RegisterForm } from './components';
 import './index.css';
 
 const App = () => {
-  return null;
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
